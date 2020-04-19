a04f309eb1463c575a1a2c031caa7fb274f1f4eb
noku-lab <61013631+noku-lab@users.noreply.github.com>
2020-04-19 10:52:20 +0200

    Updates

---
+++ precache-manifest.c9278be6b5d71a4a1a94a4f50af215fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d27cde6ff5d9dadd61d81674fe04a979",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "1cb2f9c9154aef01745c",
+    "url": "/weather-app/static/css/main.b0f1e7b0.chunk.css"
+  },
+  {
+    "revision": "8ddbb7d98f5b77cd757f",
+    "url": "/weather-app/static/js/2.336ea6e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.336ea6e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cb2f9c9154aef01745c",
+    "url": "/weather-app/static/js/main.6a86fc77.chunk.js"
+  },
+  {
+    "revision": "8ad2e2c3d0b0209b3902",
+    "url": "/weather-app/static/js/runtime-main.4479c0a4.js"
+  },
+  {
+    "revision": "bc873d2235554bf1ab42793f071f349f",
+    "url": "/weather-app/static/media/summer.bc873d22.jpg"
+  },
+  {
+    "revision": "b98d67321ab0a2187140d3a2ce4f6f89",
+    "url": "/weather-app/static/media/winter.b98d6732.jpg"
+  }
+]);
